7582725f9dd564ea02ec9778e5ad0e6913b058eb
Steven <wei.steven16@gmail.com>
2020-03-15 16:50:44 -0500

    get title and title span

--- ezrecipe/recipescraper.py
+++ ezrecipe/recipescraper.py
@@ -27,31 +27,55 @@ def get_links():
 
 
 #main function
-recipe_json = open('recipes.json', 'w')
-recipe_json.truncate()
+def main():
+    recipe_json = open('recipes.json', 'w')
+    recipe_json.truncate()
 
-output_text = open('output.txt', 'w')
-output_text.truncate()
+    output_text = open('output.txt', 'w')
+    output_text.truncate()
 
-ingredients_text = open('all_ingredients.txt', 'r')
-all_ingredients = ingredients_text.read().split("\n")
-ingredients_text.close()
+    ingredients_text = open('all_ingredients.txt', 'r')
+    all_ingredients = ingredients_text.read().split("\n")
+    ingredients_text.close()
 
-for recipe_id in range(6660, 27000):
-    
-    soup = None
-    try:
-        url = "http://allrecipes.com/recipe/{}".format(recipe_id)
-        with urllib.request.urlopen(url) as response:
-            soup = BeautifulSoup(response.read(), "html.parser")
+    for recipe_id in range(6660, 27000):
         
-    except urllib.error.HTTPError as e:
-        output_text.write("{0}: No recipe".format(recipe_id))
-        output_text.write(e.reason)
-    except urllib.error.URLError as e:
-        output_text.write("{0}: URL ERROR".format(recipe_id))
-        output_text.write(e.reason)
-    except SocketError as e:
-        output_text.write("{0}: SOCKET ERROR".format(recipe_id))
-    
-    if soup:
+        soup = None
+        try:
+            url = "http://allrecipes.com/recipe/{}".format(recipe_id)
+            response = requests.get(url)
+            response.raise_for_status()
+            soup = BeautifulSoup(response.text, "html.parser")
+            
+        except requests.exceptions.HTTPError as e:
+            output_text.write("{0}: No recipe".format(recipe_id))
+            output_text.write(e)
+        except requests.exceptions.ConnectionError as e:
+            output_text.write("{0}: CONNECTION ERROR".format(recipe_id))
+            output_text.write(e)
+        except SocketError as e:
+            output_text.write("{0}: SOCKET ERROR".format(recipe_id))
+            output_text.write(e)
+        
+        if soup:
+            title_span = soup.find("h1", class_="recipe-summary__h1")
+            serving_span = soup.find("span", class_="servings-count")
+            calorie_span = soup.find("span", class_="calorie-count")
+            direction_span = soup.find_all("span", class_="recipe-directions__list--item")
+            ingredients_object = soup.find_all("span", class_="recipe-ingred_txt")
+            footnotes_span = soup.find_all("section", class_="recipe-footnotes")
+
+        #get title
+        title = title_span.text
+
+
+        #get ingredients
+        num_ingredients = len(ingredients_objects) - 3
+        
+        for i in range(num_ingredients):
+            ingredient = {}
+            ingredient_str = ingredient_objects[i].text
+            
+
+
+
