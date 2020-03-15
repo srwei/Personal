84307bcebc4dc6a0a772c9558274c0041004d29f
Steven <wei.steven16@gmail.com>
2020-03-15 16:03:47 -0500

    starting main function to grab all recipes on allrecipes

--- ezrecipe/recipescraper.py
+++ ezrecipe/recipescraper.py
@@ -1,7 +1,11 @@
-import urllib3
+import urllib
 import re
+import json
+import requests
 from bs4 import BeautifulSoup
 from csv import DictReader, DictWriter
+from socket import error as SocketError
+from nltk.tokenize import sent_tokenize
 
 def get_ingredients():
     url = f'https://www.simplyrecipes.com/recipes/cranberry_sauce/'
@@ -20,3 +24,34 @@ def get_links():
     url = f'https://www.simplyrecipes.com/'
     req = urllib3.PoolManager()
     res = req.request('GET', url)
+
+
+#main function
+recipe_json = open('recipes.json', 'w')
+recipe_json.truncate()
+
+output_text = open('output.txt', 'w')
+output_text.truncate()
+
+ingredients_text = open('all_ingredients.txt', 'r')
+all_ingredients = ingredients_text.read().split("\n")
+ingredients_text.close()
+
+for recipe_id in range(6660, 27000):
+    
+    soup = None
+    try:
+        url = "http://allrecipes.com/recipe/{}".format(recipe_id)
+        with urllib.request.urlopen(url) as response:
+            soup = BeautifulSoup(response.read(), "html.parser")
+        
+    except urllib.error.HTTPError as e:
+        output_text.write("{0}: No recipe".format(recipe_id))
+        output_text.write(e.reason)
+    except urllib.error.URLError as e:
+        output_text.write("{0}: URL ERROR".format(recipe_id))
+        output_text.write(e.reason)
+    except SocketError as e:
+        output_text.write("{0}: SOCKET ERROR".format(recipe_id))
+    
+    if soup:
