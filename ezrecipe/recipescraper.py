27723aa364b85151384996f97866ceba000c5521
Steven <wei.steven16@gmail.com>
2020-03-17 22:19:31 -0500

    added test function

--- ezrecipe/recipescraper.py
+++ ezrecipe/recipescraper.py
@@ -77,6 +77,7 @@ def main():
     output_text.truncate()
 
     ingredients_csv = open('all_ingredients.csv', 'r')
+    all_ingredients = ingredients_csv.read().split("\n")
     ingredients_csv.truncate()
     ingredients_csv.close()
 
@@ -191,11 +192,10 @@ def main():
             del parsed_ingredient[-1]
 
         for word in parsedIngredient:
-			for suffix in hyphen_suffixes:
-				if suffix in word:
-					word=word.replace(suffix, "-" + suffix)
-				
-			for hypenatedPrefix in hyphen_prefixes:
+		    for suffix in hyphen_suffixes:
+			    if suffix in word:
+					word=word.replace(suffix, "-" + suffix)          
+            for prefix in hyphen_prefixes:
 				if word.find(prefix) == 0:
 					word=word.replace(prefix, prefix + "-")
 
@@ -205,12 +205,160 @@ def main():
 				"tea" in parsed_ingredient):
 			parsed_ingredient.remove("powder")
 
+        ingredient_str = " ".join(parsed_ingredient)
+
+        if "*" in ingredient_str:
+            ingredient_str.replace("*","")
         
+        if "," in ingredient_str:
+            ingredient_str.replace(",", "")
+
+        plural_bool = check_plurals(ingredient_str, all_ingredients)
+
+        if not plural_bool:
+            all_ingredients.append(ingredient_str)
+
+def test(id):
+
+    soup = None
+    try:
+        url = "http://allrecipes.com/recipe/{}".format(recipe_id)
+        response = requests.get(url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, "html.parser")
         
+    except requests.exceptions.HTTPError as e:
+        output_text.write("{0}: No recipe".format(recipe_id))
+        output_text.write(e)
+    except requests.exceptions.ConnectionError as e:
+        output_text.write("{0}: CONNECTION ERROR".format(recipe_id))
+        output_text.write(e)
+    except SocketError as e:
+        output_text.write("{0}: SOCKET ERROR".format(recipe_id))
+        output_text.write(e)
+    
+    if soup:
+        title_span = soup.find("h1", class_="recipe-summary__h1")
+        serving_span = soup.find("span", class_="servings-count")
+        calorie_span = soup.find("span", class_="calorie-count")
+        direction_span = soup.find_all("span", class_="recipe-directions__list--item")
+        ingredients_object = soup.find_all("span", class_="recipe-ingred_txt")
+        footnotes_span = soup.find_all("section", class_="recipe-footnotes")
+
+    #get title
+    title = title_span.text
+
+
+    #get ingredients
+    num_ingredients = len(ingredients_object) - 3
+    
+    for i in range(num_ingredients):
+        ingredient = {}
+        ingredient_str = ingredients_object[i].text
+        while True:
+            description = description_regex.search(ingredient_str)
+            if not description:
+                break
+            description_string = description.group()
+            ingredient_str.replace(description_string, "")
+        ingredient_str = ingredient_str.replace(","," and ")
+        ingredient_str = ingredient_str.replace("-"," ")
+        parsed_ingredient = ingredient_str.split(" ")
+
+        for i in range(len(parsed_ingredient)):
+            if parsed_ingredient[i] in prepositions:
+                parsed_ingredient = parsed_ingredient[:i]
+                break
+
+    
+    for i in range(0, len(parsed_ingredient)):
+        plural_unit = check_plurals(parsed_ingredient[i], measurement_units)
+        if plural_unit:
+            del parsed_ingredient[i]
+
+            if i < len(parsed_ingredient) and parsed_ingredient[i] == "+":
+                while "+" in parsed_ingredient:
+                    index = parsed_ingredient.index("+")
+                    del parsed_ingredient[index]
+            break
+    
+    for word in parsed_ingredient:
+        if word in unnecessary_words:
+            parsed_ingredient.remove(word)
+
+    index = 0
+    while index < len(parsed_ingredient):
+        descriptionString = ""
+        word = parsed_ingredient[index]
+
+        # search through descriptions (adjectives)
+        if word in descriptions:
+            descriptionString = word
+
+            # check previous word
+            if index > 0:
+                previous = parsed_ingredient[index - 1]
+                if previous in preceding_words or previous[-2:] == "ly":
+                    descriptionString = previous + " " + word
+                    parsed_ingredient.remove(previous)
+
+            # check next_word word
+            elif index + 1 < len(parsed_ingredient):
+                next_word = parsed_ingredient[index + 1]
+                if next_word in succeeding_words or next_word[-2:] == "ly":
+                    descriptionString = word + " " + next_word
+                    parsed_ingredient.remove(next_word)
+
+        # word not in descriptions, check if description with predecessor
+        elif word in description_preds and index > 0:
+            descriptionString = parsed_ingredient[index - 1] + " " + word
+            del parsed_ingredient[index - 1]
         
+        # either add description string to descriptions or check next_word word
+        if descriptionString == "":
+            index+=1
+        else:
+            parsed_ingredient.remove(word)
 
-        
+    while "and" in parsed_ingredient:
+        parsed_ingredient.remove("and")
+
+    if parsed_ingredient[-1] == "or":
+        del parsed_ingredient[-1]
+
+    for word in parsedIngredient:
+        for suffix in hyphen_suffixes:
+            if suffix in word:
+                word=word.replace(suffix, "-" + suffix)
+            
+        for hypenatedPrefix in hyphen_prefixes:
+            if word.find(prefix) == 0:
+                word=word.replace(prefix, prefix + "-")
+
+    if "powder" in parsed_ingredient and \
+        ("coffee" in parsed_ingredient or \
+            "espresso" in parsed_ingredient or \
+            "tea" in parsed_ingredient):
+        parsed_ingredient.remove("powder")
+
+    ingredient_str = " ".join(parsed_ingredient)
+
+    if "*" in ingredient_str:
+        ingredient_str.replace("*","")
+    
+    if "," in ingredient_str:
+        ingredient_str.replace(",", "")
+
+    plural_bool = check_plurals(ingredient_str, all_ingredients)
+
+    if not plural_bool:
+        all_ingredients.append(ingredient_str)
+
+    print("For recipe {}, these are the ingredients:", title)
+
+    for i in all_ingredients:
+        print(i)
 
 
 
-
+    
